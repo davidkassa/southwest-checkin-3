9a0511fb336b4cda2c2bab9f9040a7005b383a7b
Aaron Ortbals <aaron.ortbals@gmail.com>
2013-10-25 07:17:02 -0500

    create scoped session in the task

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from sqlalchemy.orm import scoped_session
 
 from settings import Config
 config = Config()
@@ -14,7 +15,7 @@ celery.config_from_object('celery_config')
 def test_celery(flight_id):
   try:
     db = Database('southwest-checkin.db')
-    flight = db.Session.query(Flight).get(flight_id)
+    flight = session.query(Flight).get(flight_id)
     return "Found flight %s" % flight.id
   except Exception, exc:
     raise test_celery.retry(exc=exc)
@@ -22,12 +23,14 @@ def test_celery(flight_id):
 @celery.task(default_retry_delay=config["RETRY_INTERVAL"], max_retries=config["MAX_RETRIES"])
 def check_in_flight(reservation_id, flight_id):
   db = Database(heroku=True)
-  flight = db.Session.query(Flight).get(flight_id)
+  session = scoped_session(db.session_factory)
+
+  flight = session.query(Flight).get(flight_id)
   if flight.success:
     print "Skipping flight %d. Already checked in at %s" % (flight_id, flight.position)
     return
 
-  reservation = db.Session.query(Reservation).get(reservation_id)
+  reservation = session.query(Reservation).get(reservation_id)
 
   (position, boarding_pass) = getBoardingPass(reservation)
 
