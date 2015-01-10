d3e3834c7f843ff5a36ecf4834103aee0e8e3a16
Aaron Ortbals <aaron.ortbals@gmail.com>
2015-01-10 10:25:13 -0600

    successful request for getTravelInfo and checkIntravelAlerts

--- lib/southwest/flight_checkin.rb
+++ lib/southwest/flight_checkin.rb
@@ -9,38 +9,54 @@ module Southwest
     attr_reader :last_name
     attr_reader :first_name
     attr_reader :record_locator
+    attr_reader :cacheid
+    attr_reader :jsessionid
 
     def initialize(last_name:, first_name:, record_locator:)
       @last_name = last_name
       @first_name = first_name
       @record_locator = record_locator
-      Southwest::Client.new.make_request(params)
     end
 
     def checkin
+      get_travel_info
       check_intravel_alerts
       flight_checkin_new
       get_all_boarding_passes
       view_boarding_passes
     end
 
+    def get_travel_info
+      response = make_request(get_travel_info_params)
+      cookies = parse_cookies(response)
+      @jsessionid = cookies['JSESSIONID'].first if cookies['JSESSIONID']
+      @cacheid = cookies['cacheid'].first if cookies['cacheid']
+      response
+    end
+
     def check_intravel_alerts
-      response = make_request(check_intravel_alerts_params)
+      make_request(check_intravel_alerts_params)
     end
 
     def flight_checkin_new
-      response = make_request(flight_checkin_new_params)
+      make_request(flight_checkin_new_params)
     end
 
     def get_all_boarding_passes
-      response = make_request(get_all_boarding_passes_params)
+      make_request(get_all_boarding_passes_params)
     end
 
     def view_boarding_passes
-      response = make_request(get_all_boarding_passes_params)
+      make_request(get_all_boarding_passes_params)
     end
 
     private
+
+    def get_travel_info_params
+      base_params.merge({
+        serviceID: 'getTravelInfo'
+      })
+    end
 
     def check_intravel_alerts_params
       base_params.merge({
@@ -72,21 +88,40 @@ module Southwest
     def base_params
       {
         appID: 'swa',
-        channe: 'rc',
+        channel: 'rc',
         appver: '2.10.0',
         platform: 'iPhone',
         cacheid: ''
       }
     end
 
+    def make_request(params)
+      Typhoeus::Request.post(base_uri, body: params, headers: headers)
+    end
+
+    def headers
+      headers = { 'User-Agent' => user_agent }
+      headers.merge('Cookie' => cookie) if cookie
+      headers
+    end
+
+    def cookie
+      cookies = []
+      cookies << "JSESSIONID=#{jsessionid}" if jsessionid
+      cookies << "cacheid=#{cacheid}" if cacheid
+      cookies.any? ? cookies.join('; ') : nil
+    end
+
+    def parse_cookies(response)
+      CGI::Cookie::parse(response.headers['Set-Cookie'].join(';'))
+    end
+
     def base_uri
       'https://mobile.southwest.com/middleware/MWServlet'
     end
 
-    def make_request(params)
-      Typhoeus::Request.new(base_uri,
-        method: :post,
-        params: params)
+    def user_agent
+      'Southwest/2.10.0 CFNetwork/711.1.16 Darwin/14.0.0'
     end
   end
 end
--- spec/lib/southwest/flight_checkin_spec.rb
+++ spec/lib/southwest/flight_checkin_spec.rb
@@ -1,11 +1,40 @@
 require 'spec_helper'
+require 'helpers/vcr_helper'
 require_relative '../../../lib/southwest/flight_checkin'
+require 'pry'
 
 describe Southwest::FlightCheckin do
-  it 'puts out the arguments' do
+  subject(:checkin) {
     Southwest::FlightCheckin.new(
       last_name: 'Foo',
       first_name: 'Bar',
       record_locator: 'ABC123')
+  }
+
+  describe '#get_travel_info initializes the checkin' do
+    let(:jsessionid) { '4A508841E779EB7E959D22737087FAE5' }
+    let(:cacheid) { '124f9430192-ab08-4426-884d-1a38f231e349' }
+
+    before do
+      VCR.use_cassette 'getTravelInfo' do
+        subject.get_travel_info
+      end
+    end
+
+    it 'sets @jsessionid from cookie' do
+      expect(subject.jsessionid).to eql(jsessionid)
+    end
+
+    it 'sets @cacheid from cookie' do
+      expect(subject.cacheid).to eql(cacheid)
+    end
+  end
+
+  describe '#check_intravel_alerts' do
+    it 'returns a response' do
+      VCR.use_cassette 'checkIntravelAlerts' do
+        expect(JSON.parse(subject.check_intravel_alerts.body)['appState']).to eql('green')
+      end
+    end
   end
 end
