a5b0945e264feaf8df969933b765b84b5eca7c8d
Aaron Ortbals <aaron.ortbals@gmail.com>
2015-01-19 23:26:10 -0600

    add form fields for new user

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,5 @@
 class Reservation < ActiveRecord::Base
   has_many :passengers
   belongs_to :user
-  accepts_nested_attributes_for :passengers
-  accepts_nested_attributes_for :user
+  accepts_nested_attributes_for :passengers, :user
 end
