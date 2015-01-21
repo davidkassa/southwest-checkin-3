d2d97a4196e5a7f0bba068caebcb793c6e1eb77c
Aaron Ortbals <aaron.ortbals@gmail.com>
2015-01-20 21:52:15 -0600

    use better names for devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :reservations
 
-  devise_for :users
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
