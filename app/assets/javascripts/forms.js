5910c357f5ed3089ccd3a18edeaad680b4e60411
Aaron Ortbals <aaron.ortbals@gmail.com>
2015-01-21 17:06:54 -0600

    highlight currently active form element

---
+++ app/assets/javascripts/forms.js
@@ -0,0 +1,32 @@
+var forms = (function() {
+  var selector = '.js-FieldGroup input';
+  var focusClass = 'field--focus';
+
+  return {
+    /**
+      Check if we are already focused
+      on page load
+    */
+    checkCurrentElement: function() {
+      if ($(document.activeElement).parents('.js-FieldGroup').addClass(focusClass)) {
+        $(document.activeElement).parent().addClass(focusClass);
+      }
+    },
+
+    highlightFormInput: function() {
+      $(selector).focusin(function(event) {
+        $(event.target).parent().addClass(focusClass);
+      });
+
+      $(selector).focusout(function(event) {
+        $(event.target).parent().removeClass(focusClass);
+      });
+    }
+  }
+})();
+
+$(document).on("ready page:load", function() {
+  console.log("HEEEE");
+  forms.checkCurrentElement();
+  forms.highlightFormInput();
+});
