8849a3b4a82011f4850a03a364417641eb291184
Aaron Ortbals <aaron.ortbals@gmail.com>
2015-01-25 21:56:26 -0600

    FlightTimeParser

---
+++ app/parsers/flight_time_parser.rb
@@ -0,0 +1,44 @@
+# This class takes a departure and date string returned by
+# the Southwest API and extracts the relevant information:
+#
+#   - UTC datetime
+#   - corresponding `Airport` record
+#   - city string
+#
+class FlightTimeParser
+  attr_reader :city_string
+  attr_reader :date_string
+
+  def initialize(city_string: city_string, date_string: date_string)
+    @city_string = city_string
+    @date_string = date_string
+  end
+
+  def utc_datetime
+    time_in_zone.utc.to_datetime
+  end
+
+  def city
+    /^\d{1,2}:\d{2}\ [APap][Mm]\ (?<city>.*)\ \([[:alpha:]]*\)/.match(city_string)[:city]
+  end
+
+  def airport
+    Airport.find_by_iata(iata)
+  end
+
+  def iata
+    /\(([[:alpha:]]*)\)/.match(city_string)[1]
+  end
+
+  private
+
+  def time_in_zone
+    Time.use_zone(airport.timezone) do
+      Time.zone.parse("#{time_string}, #{date_string}")
+    end
+  end
+
+  def time_string
+    /^\d{1,2}:\d{2}\ [APap][Mm]/.match(city_string)
+  end
+end
