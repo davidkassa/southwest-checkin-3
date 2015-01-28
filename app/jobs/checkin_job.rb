4bfce85f02aa8706628965b266b556bc8053a0ed
Aaron Ortbals <aaron.ortbals@gmail.com>
2015-01-27 20:58:54 -0600

    checkin job triggers a checkin

--- app/jobs/checkin_job.rb
+++ app/jobs/checkin_job.rb
@@ -1,7 +1,10 @@
 class CheckinJob < ActiveJob::Base
   queue_as :checkin
 
-  def perform(*args)
-    puts "Fuu bar!"
+  def perform(flight)
+    Southwest::Checkin.checkin(
+      last_name: flight.reservation.last_name,
+      first_name: flight.reservation.first_name,
+      record_locator: flight.reservation.confirmation_number)
   end
 end
--- spec/jobs/checkin_job_spec.rb
+++ spec/jobs/checkin_job_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe CheckinJob, :type => :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :reservations, :flights
+
+  let(:flight) { reservations(:denver).flights.where(position: 1).first }
+
+  it 'checks in the flight' do
+    VCR.use_cassette 'checkin' do
+      perform_enqueued_jobs do
+        CheckinJob.perform_later(flight)
+      end
+    end
+  end
 end
