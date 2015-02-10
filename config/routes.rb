fd783c4074a9b1c2c1e8814b7e7a8039570fb0d9
Aaron Ortbals <aaron.ortbals@gmail.com>
2015-02-10 11:40:35 -0600

    add the user -> reservations resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :users do
+    resources :reservations
+  end
+
   # You can have the root of your site routed with "root"
   root 'home#index'
 
