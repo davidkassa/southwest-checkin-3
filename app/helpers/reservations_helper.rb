4bbf73c7d5f2e87a939158163fa1a9b23970e347
Aaron Ortbals <aaron.ortbals@gmail.com>
2015-02-25 22:19:41 -0600

    style changes for scheduled time

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -6,4 +6,8 @@ module ReservationsHelper
   def flight_date(local_time)
     time_tag local_time, local_time.strftime('%a, %b %d')
   end
+
+  def flight_time_basic(local_time)
+    time_tag local_time, local_time.strftime('%l:%M %p')
+  end
 end
