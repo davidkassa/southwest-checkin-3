d2f74cd42fda9ca1b7905660d822361e7893f890
Aaron Ortbals <aaron.ortbals@gmail.com>
2015-02-28 11:12:43 -0600

    authenticate_admin!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def authenticate_admin!
+    authenticate_user!
+    unless current_user.admin?
+      redirect_to root_path, alert: 'You are not authorized.'
+    end
+  end
 end
