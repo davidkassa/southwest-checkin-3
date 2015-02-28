de9aaa7dc722d0cb42f283c2498b640840475c47
Aaron Ortbals <aaron.ortbals@gmail.com>
2015-02-28 00:08:33 -0600

    use an env for welcome template name

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < MandrillMailer
   def self.welcome_email(user)
     mail(to: user.email,
          subject: "Welcome to #{ENV['SITE_NAME']}",
-         template_name: 'Welcome')
+         template_name: ENV['MAIL_WELCOME_TEMPLATE'])
   end
 end
