0d14e4ea57035023f443079fdc16e5b9cfde9915
Aaron Ortbals <aaron.ortbals@gmail.com>
2015-03-04 19:44:53 -0600

    set devise ssl settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,11 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # NOTE: SSL redirect configured at the nginx level
+  # config.force_ssl = true
+  config.to_prepare { Devise::SessionsController.force_ssl }
+  config.to_prepare { Devise::RegistrationsController.force_ssl }
+  config.to_prepare { Devise::PasswordsController.force_ssl }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
