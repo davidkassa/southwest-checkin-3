2e1a5cf124f8c8001e3e134959d3bd6f27a75439
Aaron Ortbals <aaron.ortbals@gmail.com>
2016-01-28 18:26:26 -0600

    sidekiq syslogger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   config.lograge.enabled = true
   config.lograge.formatter = Lograge::Formatters::Json.new
 
+  Sidekiq::Logging.logger = ActiveSupport::TaggedLogging.new(Syslogger.new("sidekiq", Syslog::LOG_PID, Syslog::LOG_LOCAL7))
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
